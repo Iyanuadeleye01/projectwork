271ce75b8980eda992b715dc26c1cb2dc9809582
Iyanu Adeleye <adeley1106@gmail.com>
2023-10-27 11:22:44 +0100

    Show theri status

--- payment.py
+++ payment.py
@@ -1,5 +1,11 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_csv("Advertising.csv")
-df
-    
+firstnames = ('pedro', 'toyin', 'dayo','iyanu')
+lastnames = ('oye','ojo','bello','tosin')
+ages = (34,43,34,23)
+total_age =sum(ages)
+average_age = total_age/len(ages)
+for firstname, lastname, age in zip(firstnames,lastnames,ages):
+    if age > average_age:
+        print(f'{firstname} {lastname} is old')
+    else:
+        print(f' {firstname} {lastname} is young')
+        
